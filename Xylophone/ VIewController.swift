32b32c8c946f7f30fae6601546dd84c6765611be
Vladislav <muriginvlad@gmail.com>
2019-08-22 12:20:52 +0300

    Initial Commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Владислав on 25/06/2019.
+//  Copyright © 2019 Murygin Vladislav. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioPlayer: AVAudioPlayer!
+    let note = ["note1","note2","note3","note4","note5","note6","note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+
+        playSound(note[sender.tag - 1])
+
+     }
+    
+    
+    func playSound(_ sound: String) {
+        
+        let soundURL = Bundle.main.url(forResource: "\(sound)", withExtension: "wav")
+        
+        do {try audioPlayer =  AVAudioPlayer(contentsOf: soundURL!)}catch{print(error)}
+        
+        audioPlayer.play()
+    }
+    
+}
