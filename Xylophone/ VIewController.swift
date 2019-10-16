e88d719be7d6e2380fbb9539a36159fe51674051
Vladislav <muriginvlad@gmail.com>
2019-10-16 21:30:29 +0300

    Added opacity change when button is clicked

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -19,19 +19,22 @@ class ViewController: UIViewController{
     }
     
     @IBAction func notePressed(_ sender: UIButton) {
-
+        
         playSound(note[sender.tag - 1])
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        sender.alpha = 1
+        }
 
-     }
-    
+        }
     
     func playSound(_ sound: String) {
         
         let soundURL = Bundle.main.url(forResource: "\(sound)", withExtension: "wav")
         
         do {try audioPlayer =  AVAudioPlayer(contentsOf: soundURL!)}catch{print(error)}
-        
+    
         audioPlayer.play()
     }
-    
 }
